cd097c83fbe0ab809d7820f699e6464f4564ab49
Nouzbe <benjamin.amelot@gmail.com>
2022-01-05 16:48:09 +0100

    Remove typescript

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 function run() {
   const body = github.context.payload.pull_request?.body;
